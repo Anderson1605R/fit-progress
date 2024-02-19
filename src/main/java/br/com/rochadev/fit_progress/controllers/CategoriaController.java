23ffefcb03548e33442ea5ae9c912520df0a9677
Anderson1605R <114451515+Anderson1605R@users.noreply.github.com>
2024-02-19 20:26:53 -0300

    Criação do controller de categoria

---
+++ src/main/java/br/com/rochadev/fit_progress/controllers/CategoriaController.java
@@ -0,0 +1,42 @@
+package br.com.rochadev.fit_progress.controllers;
+
+import br.com.rochadev.fit_progress.model.CategoriaModel;
+import br.com.rochadev.fit_progress.repositories.CategoriaRepo;
+import br.com.rochadev.fit_progress.services.CategoriaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categoria")
+public class CategoriaController {
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaModel>> listaDeCategorias() {
+        return ResponseEntity.ok(categoriaService.buscarTodasCategorias());
+    }
+
+    @PostMapping("/salvar/{id}")
+    public ResponseEntity<CategoriaModel> salvar(@RequestBody CategoriaModel categoriaModel, @PathVariable("id") long id) {
+        categoriaService.salvarCategoria(categoriaModel, id);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<CategoriaModel> atualizar(@RequestBody CategoriaModel categoriaAtualizada, @PathVariable("id") long id){
+        return ResponseEntity.ok(categoriaService.atualizarCategoria(categoriaAtualizada, id));
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletar(@PathVariable("id") long id){
+        categoriaService.apagarCategoria(id);
+    }
+}
